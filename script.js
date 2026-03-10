4634676212048cb4d2ab6b1a030aa4b559a8dd82
VittorioDauria94 <dauriavittorio94@gmail.com>
2026-03-10 10:59:03 +0100

    feat: snack 5 done

--- script.js
+++ script.js
@@ -102,8 +102,26 @@ const agesSum = ages.reduce((acc, curr) => acc + curr, 0);
 
 // Stampa in console l’età media degli autori dei libri.
 
-const averageAge = agesSum / ages.length
+const averageAge = agesSum / ages.length;
 
 console.log("Average age:", averageAge);
 
+// Snack 5 (Bonus)
+// Usando la l'API http://localhost:3333/books/{id} usa la combinazione di .map() e Promise.all(), per creare una funzione (getBooks) che a partire da un array di id (ids), ritorna una promise che risolve un array di libri (books).
+// Testala con l’array [2, 13, 7, 21, 19] .
 
+async function getBooks(ids) {
+        const promises = ids.map((id) =>
+          fetch(`http://localhost:3333/books/${id}`).then((res) => res.json()),
+        );
+        return Promise.all(promises);
+}
+
+(async () => {
+  try {
+    const books = await getBooks([2, 13, 7, 21, 19]);
+    console.log(books);
+  } catch (error) {
+    console.error(error);
+  }
+})();
